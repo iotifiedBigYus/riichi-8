1ef6f5f9799998137cf5c194413c8c301f5a9ad3
iotifiedBigYus <sam.besterlund@gmail.com>
2026-06-03 22:38:11 +0300

    refactor: replace old discard drawing functions

--- src/main.lua
+++ src/main.lua
@@ -10,22 +10,16 @@ end
 
 function _draw()
 	cls(1)
-	--draw_discards(test_discards())
-	--draw_discards(test_discards(),2)
-	--draw_discards(test_discards(),3)
-	--draw_discards(test_discards(),4)
 	color(6)
 	print_hand(test_hand())
 	print_hand(test_hand2())
 	print_hand(sort_hand(test_hand2()))
 	print(analysis.is_terminal_or_honor(11))
 
-	draw_tile2(19, 10, 10)
-
-	draw_discards2(test_discards3())
-	draw_discards2(test_discards3(),2)
-	draw_discards2(test_discards3(),3)
-	draw_discards2(test_discards3(),4)
+	draw_discards(test_discards3())
+	draw_discards(test_discards3(),2)
+	draw_discards(test_discards3(),3)
+	draw_discards(test_discards3(),4)
 
 	function a()
 		return 1,2
