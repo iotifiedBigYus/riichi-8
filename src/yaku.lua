c80c5be24ada5c359a09d544eaaa2e88ef56b9e0
iotifiedBigYus <sam.besterlund@gmail.com>
2026-06-07 21:17:29 +0300

    feat: make yaku functions templates

---
+++ src/yaku.lua
@@ -0,0 +1,76 @@
+
+
+function is_tenpai(hand_state, drawn_tile)
+	return bool, winning_tiles
+end
+
+function is_ronnable(hand_state, opp_discarded_tile)
+	return true, yakus
+end
+
+function is_tsumoable(hands_state, drawn_tile)
+	return true, yakus
+end
+
+function get_han(yakus, is_open)
+
+end
+
+function get_fu(hand_state, last_tile)
+
+end
+
+function get_score(han, fu)
+
+end
+
+
+
+
+
+
+
+
+
+
+function find_all_yakus(tiles, melds, seat_wind, round_wind, is_closed, is_riichi, is_first_tile, is_first_discard)
+	-- tiles = {1,2,3,4, ... ,0,0,0,0}
+
+
+	local score = get_score(han, fu)
+	return yakus, han, fu, score
+end
+
+
+function get_score(han, fu)
+	local points
+	return points
+end
+
+--[[
+
+tiles = {0,0,0,....,0,0,0}
+melds = {
+	{1,1,1,0,0,0,...,0,0},
+	{0,0,0, ... ,3, ..., 0}
+	{0,0,0, ... ,4, ..., 0}
+}
+drawn_tile = 20
+discarded_tile = 20
+
+
+n_tiles = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+
+i_tiles = {}
+
+n_tiles = {3,3,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+
+i_tiles = {1,2,3}
+
+
+
+
+
+
+
+]]
