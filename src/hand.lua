9e8c670eb3b829cab9905cafdacf8731b247cee5
iotifiedBigYus <sam.besterlund@gmail.com>
2026-07-21 00:51:10 +0300

    feat: make drawable hand object

--- src/hand.lua
+++ src/hand.lua
@@ -1,29 +1,102 @@
 -- hand
 
 
-assert(util)
-assert(class)
+assert(entity)
+assert(large_tile)
 assert(meld)
 assert(meld_stack)
 
 
-hand = class:subclass{
-	--drawn_tile = nil,
+hand = entity:subclass{
+	--length = 0,
+	--is_open = false,
+
 	new = function(self)
-		return self:subclass({
-			n_tiles = empty_tiles(),
-			n_melds = {},
-			meld_stack = global.meld_stack:new(),
+		return entity.new(self,{
+			tiles = {},
+			--tile_states = {},
+			--[[
+				state: desired {x, y, rotation, status}
+				for each tile in all_tiles
+			]]
 		})
 	end,
 
-	draw = function(_ENV)
+	set_tiles = function(_ENV, new_tiles)
+		tiles = new_tiles
+		_ENV:update()
+		return _ENV
+	end,
 
+	set_openness = function(_ENV, new_is_open)
+		is_open = new_is_open
+		_ENV:update()
+		return _ENV
+	end,
+
+	add_tile = function(_ENV, tile)
+		add(tiles, tile)
+		_ENV:update()
+		return _ENV
+	end,
+
+	remove_tile = function(_ENV, tile)
+		del(tiles, tile)
+		_ENV:update()
+		return _ENV
+	end,
+
+	apply_tile_states = function(_ENV)
+		for i,tile in ipairs(tiles) do
+			tile:set_state(tile_states[i])
+		end
+		return _ENV
+	end,
+
+	get_tile_state = function(_ENV, i)
+		local tile_x, tile_y = _ENV:get_rotated_pos(
+			(i-.5*length-1)*6,
+			is_open and 4 or 2
+		)
+		--]]
+		return {
+			tile_x,
+			tile_y,
+			rotation,
+			is_open and 1 or 3,
+		}
+	end,
+
+	update = function(_ENV)
+		length = #tiles
+
+		for i = 1,length do
+			local j = i
+			while j > 1
+			and tiles[j-1].relative_value > tiles[j].relative_value do
+				tiles[j],tiles[j-1] = tiles[j-1],tiles[j]
+				j-=1
+			end
+		end
+
+		tile_states = {}
+		for i = 1,length do
+			add(tile_states, _ENV:get_tile_state(i))
+		end
+
+		return _ENV
+	end,
+
+	draw = function(_ENV)
+		foreach(tiles, function(tile) tile:draw() end)
+		meld_stack:draw()
+		return _ENV
 	end,
 }
 
 
 function print_hand(hand)
+	--trim
 	-- s t c o a
 	-- l a r
 	local symbol = {
@@ -69,24 +142,4 @@ function print_hand(hand)
 
 	print(out)
 	return out
-end
-
-
-function sort_hand(hand)
-	-- mutates the hand
-	local tiles = hand.tiles
-
-	for i=1,#tiles do
-		tiles[i].prev_pos = i
-	end
-	
-	for k,v in ipairs(tiles) do
-		local j = k
-		while j > 1 and tiles[j-1].number > tiles[j].number do
-			tiles[j],tiles[j-1] = tiles[j-1],tiles[j]
-			j = j - 1
-		end
-	end
-
-	return hand
 end
